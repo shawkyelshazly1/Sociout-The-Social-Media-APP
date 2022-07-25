2aaefc634c2a99da4d8edc6c1fc77da030ca836d
Shaq <shawkyelshazly2@gmail.com>
2022-07-25 13:56:45 +0200

    added register route & saving user to DB

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,32 @@
-const router = require("express").Router();
+const { default: mongoose } = require("mongoose");
 
-router.get("/", (req, res) => {
-  res.send("From auth page");
+const router = require("express").Router(),
+  User = require("../models/User"),
+  bcrypt = require("bcrypt");
+
+// Register route
+router.post("/register", async (req, res) => {
+  try {
+    // hashing the password using bcrypt
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // creating new user object from mongoose mdoel
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // saving user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+
+
 
 module.exports = router;
